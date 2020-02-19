f4fa24af967b74b24e786ea56883101a26f389a6
Pablo <juanpa92008@gmail.com>
2020-02-19 13:18:51 -0600

    Leccion 15 Componentes funcionales

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name= 'Pablo Lozano'
-const user={name:'Pablo Lozano',
-            age:'20',
-            country:'México'}
+// Los componentes siempre deben de comenzar con Mayúscula
+// De retornarse solamente un elemento, realizarlo solo en una línea, de lo contrario utilizar paréntesis para una mejor indentación
+        const TarjetaFruta = () => (
+            <div>
+                <h3>Titulo</h3> 
+                <hr/>
+                <p>Descripción</p>
+            </div>  
+        )
 
-function getinfo(user)
-{
-    return `Hola mi nombre es ${user.name} y tengo ${user.age} años, el doble de mi edadd es ${user.age * 2}` 
-}
+// TarjetaFruta también se puede declarar de la siguiente manera
+        function TarjetaFruta2(){
+            return 
+            <div>
+                <h3>Titulo</h3> 
+                <hr/>
+                <p>Descripción</p>
+            </div>  
+        }
 
-const App = <h1>Hola React!*** {getinfo(user)} </h1>;
+const App = ()=>
+    (
+        <div>
+            {/* Es lo mismo */}
+            <TarjetaFruta/>
 
-ReactDOM.render(App,document.getElementById('root'));
+            {/* A esto */}
+            <TarjetaFruta></TarjetaFruta>
+
+            <TarjetaFruta2/>   
+        </div>
+    )
+
+ReactDOM.render(<App/>,document.getElementById('root'));
