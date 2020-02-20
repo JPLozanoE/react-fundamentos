25b018ab5e3615415ab238c52b904ecfd1319714
Pablo <juanpa92008@gmail.com>
2020-02-20 15:18:09 -0600

    Componentes de clase y estado

--- src/index.js
+++ src/index.js
@@ -1,44 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Los componentes siempre deben de comenzar con Mayúscula
-// De retornarse solamente un elemento, realizarlo solo en una línea, de lo contrario utilizar paréntesis para una mejor indentación
-        const TarjetaFruta = (props) => (
+
+// Componente de clase extender clase React.component y hacer uso del método render para regresar elementos
+// Los componentes estatales se pueden modificar y actualizar utilizando ESTADO
+class TarjetaFruta extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            cantidad:5
+        }
+    }
+    render(){
+        return (
             <div>
-                <h3>{props.name}</h3> 
-        <p>${props.price}</p>
+                <h3>{this.props.name}</h3> 
+        <div>Cantidad: {this.state.cantidad}</div>
+                <button 
+                onClick={() => {
+                    // this.state.cantidad=100;
+                    this.setState({cantidad: this.state.cantidad+1});
+                    // El uso de forceupdate() no es una buena práctica
+                    // this.forceUpdate();
+                    }}>
+                Agregar
+                </button>
+                <p>${this.props.price}</p>
                 <hr/>
             </div>  
         )
-
-// TarjetaFruta también se puede declarar de la siguiente manera
-        function TarjetaFruta2(props){
-            console.log(props)
-            return <div>
-                <h3>{props.name}</h3> 
-                <p>${props.price}</p>
-                <hr/>
-            </div>  
-        }
-
+    }
+}
         var FrutaExotica='Fruta de Dragon'
-
-        // Componente Funcional
 const App = ()=>
     (
         <div>
             {/* Es lo mismo */}
             {/* Para pasar props es necesario utilizar llaves SIEMPRE, a excepción de cuando se pasa un string, en ese caso las llaves son opcionales */}
-            <TarjetaFruta name='Sandía' price={3.50}/>
-            <TarjetaFruta name='Naranja' price={1.00} />
-            <TarjetaFruta name='kiwi' price={1.00} />
-            <TarjetaFruta name='Plátano' price={2.00} />
+            <TarjetaFruta name='Sandía' price={3.45}/>
+            <TarjetaFruta name='Naranja' price={1.20} />
+            <TarjetaFruta name='kiwi' price={1.25} />
+            <TarjetaFruta name={`${FrutaExotica}`} price={2.30} />
 
-            {/* Uso de variables y string template, nótese el uso de Llaves */}
-            <TarjetaFruta2 name={`${FrutaExotica}`} price={5.00} />
-
-            {/* A esto */}
-            {/* <TarjetaFruta></TarjetaFruta> */}
 
         </div>
     )
