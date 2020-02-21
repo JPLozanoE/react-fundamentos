fa6adc4bdafe70015031f01e030bb4ed9fec210d
Pablo <juanpa92008@gmail.com>
2020-02-21 14:59:22 -0600

    Inicializadores de propiedad

--- src/index.js
+++ src/index.js
@@ -1,79 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Eliminamos el constructor y cambiamos de utilizar metodos a arrow functions para así evitar el bind, ambas formas son correctas. A esta manera de trabajar se le llama utilizar inicializadores de propiedad.
 
-// Componente de clase extender clase React.component y hacer uso del método render para regresar elementos
-// Los componentes estatales se pueden modificar y actualizar utilizando ESTADO
+// LOS INICIALIZADORES DE PROPIEDAD SON UTILIZADOS PERO NO SON ESTÁNDAR, LA MANERA MÁS CORRECTA DE TRABAJAR ES LA VISTA EN LA LECCIÓN ANTERIOR
 class TarjetaFruta extends React.Component{
-    constructor(){
-        super()
-        this.state={
-            cantidad:5
-        }
-
-        const METHODS = [
-            'agregar',
-            'quitar',
-            'limpiar'
-        ]
-
-        METHODS.forEach((method)=>{
-            this[method]=this[method].bind(this)
-        })
+    state={
+        cantidad:5
     }
-    agregar(){
-        // De esa manera no funciona porque apunta al metodo agregar y no a la clase TarjetaFruta
-        // this.setState({cantidad: this.state.cantidad+1});
-        this.setState({
-            cantidad: this.state.cantidad+1
-        })
-    }
+    // El método puede funcionar así
+    agregar=()=>this.setState({cantidad: this.state.cantidad+1})
+    
+    // o así
+    // agregar=()=>this.setState({
+    //     cantidad: this.state.cantidad+1
+    // })
 
-    quitar(){
-        // De esa manera no funciona porque apunta al metodo agregar y no a la clase TarjetaFruta
-        // this.setState({cantidad: this.state.cantidad+1});
-        this.setState({
-            cantidad: this.state.cantidad-1
-        })
-    }
+    quitar=()=>this.setState({cantidad: this.state.cantidad-1})
 
-    limpiar(){
-        // De esa manera no funciona porque apunta al metodo agregar y no a la clase TarjetaFruta
-        // this.setState({cantidad: this.state.cantidad+1});
-        this.setState({
-            cantidad: 0
-        })
-    }
+    limpiar=()=>this.setState({cantidad: 0})
 
     render(){
         return (
-            <div>
-                <h3>{this.props.name}</h3> 
                 <div>
-                    Cantidad: {this.state.cantidad}
-                </div>
-                <button 
-                //Las razones por las que no se utiliza un arrow function es porque .setState ejecuta el metodo Render y React hace la comparación para verificar qué fue lo modificado. Ello en un proyecto con pocos componentes tal vez no represente mucho problema pero en uno con cientos o miles de componentes puede generar un problema.
-                onClick={
-                    // Esta línea es igual a la de abajo, solamente que el .bind(this) se mueve al constructor para que este pueda funcionar de manera más organizada
-                    // this.agregar.bind(this)}>
-                    this.agregar}>
-                +
-                </button>
-                <button 
-                onClick={
-                    this.quitar}>
-                -
-                </button>
-                <button 
-                onClick={
-                    this.limpiar}>
-                Limpiar
-                </button>
-                <p>${this.props.price}</p>
-                <hr/>
-            </div>  
-        )
+                    <h3>{this.props.name}</h3> 
+                    <div>
+                        Cantidad: {this.state.cantidad}
+                    </div>
+                    <button 
+                    onClick={
+                        this.agregar}>
+                    +
+                    </button>
+                    <button 
+                    onClick={
+                        this.quitar}>
+                    -
+                    </button>
+                    <button 
+                    onClick={
+                        this.limpiar}>
+                    Limpiar
+                    </button>
+                    <p>${this.props.price}</p>
+                    <hr/>
+                </div>  
+            )
     }
 
 }
@@ -81,14 +53,10 @@ class TarjetaFruta extends React.Component{
 const App = ()=>
     (
         <div>
-            {/* Es lo mismo */}
-            {/* Para pasar props es necesario utilizar llaves SIEMPRE, a excepción de cuando se pasa un string, en ese caso las llaves son opcionales */}
             <TarjetaFruta name='Sandía' price={3.45}/>
             <TarjetaFruta name='Naranja' price={1.20} />
             <TarjetaFruta name='kiwi' price={1.25} />
             <TarjetaFruta name={`${FrutaExotica}`} price={2.30} />
-
-
         </div>
     )
 
