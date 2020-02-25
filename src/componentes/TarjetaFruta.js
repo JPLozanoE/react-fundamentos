604547bd36364226a6fa9e243848201052a9fa0a
Pablo <juanpa92008@gmail.com>
2020-02-25 14:41:30 -0600

    24-25 Estilos con clases CSS + Modulos CSS

--- src/componentes/TarjetaFruta.js
+++ src/componentes/TarjetaFruta.js
@@ -1,39 +1,30 @@
 import React from 'react'
-// Este fichero (componentes) quedó como directorio para reutilizaación de componentes
-// ESTE COMPONENTE ES UN COMPONENTE ESTATAL O DE CLASE, CON ESTADO
+// import './TarjetaFruta.css'
+// Al importar styles de esta manera guardamos en el objeto styles las clases de estilo, el punto importante es el uso de la palarbra MODULE. Esto se usa para evitar la sobreescritura de clases.
+import styles from './TarjetaFruta.module.css'
+
 class TarjetaFruta extends React.Component{
     state={
         cantidad:0
     }
     // El método puede funcionar así
     agregar=()=>this.setState({cantidad: this.state.cantidad+1})
-    
-    // o así
-    // agregar=()=>this.setState({
-    //     cantidad: this.state.cantidad+1
-    // })
 
     quitar=()=>this.setState({cantidad: this.state.cantidad-1})
 
     limpiar=()=>this.setState({cantidad: 0})
 
     render(){
-        // Declaración de lógica para determinación de colores en CSS. Separar lógica del operador. cuando esté la variable en el operador solamente debe de compararse si la variable que contiene la condición es true or false
         const hasItems =this.state.cantidad>0
-        const styles={
-            border: '1px solid black',
-            // Utilizar Camel Case para propiedades CSS. Otra alternativa es colocar comillas a las propiedades
-            marginBottom: '1em',
-            'border-radius': '0.5em',
-            // IMPORTANTE, EL PADDING HACE MARGEN HACIA DENTRO. IMPORTANTE APRENDER A USAR CSS PINCHE MENSO
-            padding: '1em',
-            // USO DE OPERADORES TERNARIOS. EL OPERADOR TERNARIO HACE UN TOGGLE ENTRE DATOS EN CASO DE CUMPLIRSE O NO CUMPLIRSE UNA CONDICIÓN
-            background: hasItems ? 'linear-gradient(45deg,black,#4a02f7)' : '#FFF',
-            color: hasItems ? '#FFF' : '#000',
-            transition: 'all 400ms ease-out'
-        }
+        // Separación de la lógica en una variable aparte.
+        const activeClass=hasItems ? styles['card-active'] : '';
+        //Utilizamos llaves y String templates para hacer un toggle entre TarjetaFruta y TarjetaFruta TarjetaFruta-activa en la clase de CSS
+        const clases=   `${styles.card} ${activeClass}`
+        // Otra manera de hacerlo es la siguiente (La manera tradicional):
+        // const clases=   styles.card + activeClass
+
         return (
-                <div style={styles}>
+                <div className={clases}>
                     <h3>{this.props.name}</h3>
                     <div>
                         Cantidad: {this.state.cantidad}
