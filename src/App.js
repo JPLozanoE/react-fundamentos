a79586837aa5894dc412634e963d51e09ad99431
Pablo <juanpa92008@gmail.com>
2020-02-28 13:33:06 -0600

    Eventos personalizados. Pasar información del hijo al padre.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import React from 'react';
 import { Component } from 'react';
-//Manejando eventos, tipos de evento y contenido de eventos.
+class Hijo extends Component {
 
-    class PersistenciaEventos extends Component
-    {
-        state={
-            color:'blue'
-        }
-        manejador = (e) =>
-        {
-            const color = e.target.value
-            this.setState( state => ({
-                color: color
-            })
-            )
-        }
-
-        render(){
-            return(
-                <div>
-                    <h1 style={{color:this.state.color}}>{this.state.color}</h1>
-                    <input type="text"
-                    onChange={this.manejador}
-                    />
-                </div>
-            )
-        }
+    // El manejador de click envía las props al evento personalizado onSaluda
+    manejadorClick = () => {
+      this.props.onSaluda('Hola Pablo','M','23')
+    }
+  
+    render () {
+      return (
+        <div className='box blue'>
+          <h2>Hijo</h2>
+          {/* El evento onClick del botón dispara el manejador de CLick */}
+          <button
+            onClick={this.manejadorClick}
+          >
+            Saluda
+          </button>
+        </div>
+      )
+    }
+  }
+  
+//   Las propiedades del estado deben ser definidas.
+  class App extends Component {
+    state = {
+      name: '',
+      age: '',
+      sex: ''
     }
 
- const App = () => (
-     <div>
-         <PersistenciaEventos/>
-     </div>
- )
+    // El manejador recibre las props y cambia el estado
+    manejador = (name,age,sex) => {
+      this.setState({ name, age, sex })
+    }
+  
+    render () {
+      return (
+        <div className='box red'>
+          <Hijo
+        //   El evento personalizado onSaluda recurre a su propio manejador
+            onSaluda={this.manejador}
+          />
+          <h1>
+            Nombre: { this.state.name }
+          </h1>
+          <h1>
+            Edad: { this.state.age }
+          </h1>
+          <h1>
+            Sexo: { this.state.sex }
+          </h1>
+        </div>
+      )
+    }
+  }
 
     export default App
