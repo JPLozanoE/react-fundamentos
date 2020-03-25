b887cd36288e417417134493ab029809e72b94aa
Pablo <juanpa92008@gmail.com>
2020-03-25 13:55:38 -0500

    Iterando listas con react

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
-import React from 'react';
-import { Component } from 'react';
-const Saludo = (props) => {
-    return (
+import React,{Component} from 'react';
+  
+const frutas=[
+  'kiwi',
+  'manzana',
+  'coco',
+  'naranja',
+  'fresa',
+  'arándano'
+]
+class App extends Component
+{
+  render(){
+    return(
       <div>
-        <div>
-            {/* Si dentro de la prop name viene llena, entonces realizar <strong>{ props.name }</strong> */}
-          { props.name && <strong>{ props.name }</strong>}
-        </div>
-        
-        {/* si props trae consigo saluda */}
-        { props.saluda
-        
-          ? (
-              // Entonces retorna esto
-            <h1>Hola, tu eres genial !</h1>
-          )
-          : (
-            //   De lo contrario retorna esto
-            <p>
-              Woops, no hay saludo para ti!
-            </p>
-          )
-        }
+        <ul>
+          {frutas.map((fruta)=>{
+            return(
+            <li>{fruta}</li>
+            )
+          })}
+        </ul>
       </div>
     )
-  
-    // if (props.saluda) {
-    //   return (
-    //     <h1>Hola, tu eres genial !</h1>
-    //   )
-    // }
-  
-    // return (
-    //   <p>
-    //     Woops, no hay saludo para ti!
-    //   </p>
-    // )
   }
-  
-  const App = () => (
-    <div>
-      <Saludo
-    //   Se envía la prop saluda. esta es recibida como true.
-        saluda
-        // Se envía la prop name con el valor 'Pablo'
-        name='Pablo'
-      />
-    </div>
-  )
-
+}
     export default App
